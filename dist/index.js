91526392945c2d0f0ef4f2a192a1e2e443622795
StephaneC <Stephane.castrec@gmail.com>
2020-01-16 20:40:49 +0100

    Update index exported

--- dist/index.js
+++ dist/index.js
@@ -2,4 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var PlateForm_1 = require("./PlateForm");
 exports.Plateform = PlateForm_1.Plateform;
+var template_utils_1 = require("./template.utils");
+exports.audioFormat = template_utils_1.audioFormat;
+exports.isAudio = template_utils_1.isAudio;
+exports.addSpeakBalise = template_utils_1.addSpeakBalise;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export { Plateform } from './PlateForm';
+export { audioFormat, isAudio, addSpeakBalise } from './template.utils'
