ffe617e03a8ea3337bc3746f2498494da8fd2c94
StephaneC <Stephane.castrec@gmail.com>
2020-01-30 15:39:30 +0100

    Export PLATEFORM

--- dist/index.js
+++ dist/index.js
@@ -6,6 +6,7 @@ var ActionTemplate_1 = require("./action/ActionTemplate");
 exports.ActionTemplate = ActionTemplate_1.ActionTemplate;
 var PlateForm_1 = require("./PlateForm");
 exports.Plateform = PlateForm_1.Plateform;
+exports.PLATEFORM = PlateForm_1.PLATEFORM;
 var PlateFormMock_1 = require("./mock/PlateFormMock");
 exports.PlateformMock = PlateFormMock_1.PlateformMock;
 var template_utils_1 = require("./template.utils");
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export { SkillTemplate } from './skill/SkillTemplate';
 export { ActionTemplate } from './action/ActionTemplate';
-export { Plateform } from './PlateForm';
+export { Plateform, PLATEFORM } from './PlateForm';
 export { PlateformMock  } from './mock/PlateFormMock';
 export { audioFormat, isAudio, addSpeakBalise } from './template.utils';
