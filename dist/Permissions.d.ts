dece7848cccf6ed77e47f4a43a03ed300f496663
StephaneC <Stephane.castrec@gmail.com>
2020-09-03 21:08:24 +0200

    Add logs for travis tsc version

--- dist/Permissions.d.ts
+++
@@ -1,7 +0,0 @@
-export declare enum PERMISSIONS {
-    GEOLOCATION = 0
-}
-export interface Permissions {
-    askPermission(permission: PERMISSIONS, text: string): any;
-    hasPermission(permission: PERMISSIONS): boolean;
-}
--- dist/skill/SkillResponseInterceptor.d.ts
+++
@@ -1,3 +0,0 @@
-import { ResponseInterceptor } from 'ask-sdk-core';
-export declare const PersistenceSavingResponseInterceptor: ResponseInterceptor;
-export declare const LastResponseSavingResponseInterceptor: ResponseInterceptor;
